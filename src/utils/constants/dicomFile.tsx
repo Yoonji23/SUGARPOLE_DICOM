eeed1e4a785fc84a1cd089d45a0181437b71ff8a
djwjfk <grw07236@naver.com>
2024-07-27 18:37:43 +0900

    Add: DICOM 이미지 추가

---
+++ src/utils/constants/dicomFile.tsx
@@ -0,0 +1,10 @@
+const dicomImg = [
+  "dicom_img_01.dcm",
+  "dicom_img_02.dcm",
+  "dicom_img_03.dcm",
+  "dicom_img_04.dcm",
+  "dicom_img_05.dcm",
+  "dicom_img_06.dcm",
+];
+
+export default dicomImg;
